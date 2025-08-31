3bb0fff6d00d128a1c3b90cec98f251a021900ad
alikhan0616 <ak098boy@gmail.com>
2025-08-31 21:49:22 +0500

    feat: implement welcome screen and navigation setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,42 @@
-import { View } from "react-native";
+import { useRouter } from "expo-router";
+import * as SystemUI from "expo-system-ui";
+import React, { useEffect } from "react";
+import { Image, StyleSheet, View } from "react-native";
+import { colors } from "../constants/theme";
 
-const index = () => {
-  return <View></View>;
+// Set navigation bar to transparent
+SystemUI.setBackgroundColorAsync("transparent");
+
+const Index = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/(auth)/welcome");
+    }, 2000);
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        resizeMode="contain"
+        source={require("../assets/images/splashImage.png")}
+      />
+    </View>
+  );
 };
 
-export default index;
+export default Index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.neutral900,
+  },
+  logo: {
+    height: "20%",
+    aspectRatio: 1,
+  },
+});
