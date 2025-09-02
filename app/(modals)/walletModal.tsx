a0916f55b519c79928d652a4e3d756af322cb0c0
alikhan0616 <ak098boy@gmail.com>
2025-09-02 20:04:21 +0500

    feat: Add wallet management modal and related services for creating/updating wallets

---
+++ app/(modals)/walletModal.tsx
@@ -0,0 +1,145 @@
+import BackButton from "@/components/BackButton";
+import Button from "@/components/Button";
+import Header from "@/components/Header";
+import ImageUplaod from "@/components/ImageUplaod";
+import Input from "@/components/Input";
+import ModalWrapper from "@/components/ModalWrapper";
+import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { useAuth } from "@/contexts/authContext";
+import { createOrUpdateWallet } from "@/services/walletService";
+import { WalletType } from "@/types";
+import { scale, verticalScale } from "@/utils/styling";
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { Alert, ScrollView, StyleSheet, View } from "react-native";
+const WalletModal = () => {
+  const { user, updateUserData } = useAuth();
+  const router = useRouter();
+  const [wallet, setWallet] = useState<WalletType>({
+    name: "",
+    image: null,
+  });
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async () => {
+    let { name, image } = wallet;
+    if (!name.trim() || !image) {
+      Alert.alert("Wallet", "Please fill all the fields");
+      return;
+    }
+
+    const data: WalletType = {
+      name,
+      image,
+      uid: user?.uid,
+    };
+
+    // wallet id
+    setLoading(true);
+    const res = await createOrUpdateWallet(data);
+    setLoading(false);
+    if (res.success) {
+      router.back();
+    } else {
+      Alert.alert("Wallet", res.msg);
+    }
+  };
+
+  return (
+    <ModalWrapper>
+      <View style={styles.container}>
+        <Header
+          title="New Wallet"
+          leftIcon={<BackButton />}
+          style={{ marginBottom: spacingY._10 }}
+        />
+
+        {/* Form */}
+        <ScrollView contentContainerStyle={styles.form}>
+          <View style={styles.inputContainer}>
+            <Typo color={colors.neutral200}>Name</Typo>
+            <Input
+              placeholder="Name"
+              value={wallet.name}
+              onChangeText={(value) => {
+                setWallet({ ...wallet, name: value });
+              }}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Typo color={colors.neutral200}>Wallet Icon</Typo>
+            {/* Image Input */}
+            <ImageUplaod
+              file={wallet.image}
+              onClear={() => setWallet({ ...wallet, image: null })}
+              onSelect={(file) => setWallet({ ...wallet, image: file })}
+              placeholder="Upload Image"
+            />
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.footer}>
+        <Button loading={loading} onPress={onSubmit} style={{ flex: 1 }}>
+          <Typo color={colors.black} fontWeight={"700"}>
+            Add Wallet
+          </Typo>
+        </Button>
+      </View>
+    </ModalWrapper>
+  );
+};
+
+export default WalletModal;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: spacingX._20,
+    justifyContent: "space-between",
+  },
+  footer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingHorizontal: spacingX._20,
+    gap: scale(12),
+    paddingTop: spacingY._15,
+    borderTopColor: colors.neutral700,
+    marginBottom: spacingY._5,
+    borderTopWidth: 1,
+  },
+  form: {
+    gap: spacingY._30,
+    marginTop: spacingY._15,
+  },
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  avatar: {
+    alignSelf: "center",
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: 200,
+    borderWidth: 1,
+    borderColor: colors.neutral500,
+  },
+  editIcon: {
+    position: "absolute",
+    bottom: spacingY._5,
+    right: spacingY._7,
+    borderRadius: 100,
+    backgroundColor: colors.neutral100,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: spacingY._7,
+  },
+  inputContainer: {
+    gap: spacingY._10,
+  },
+});
--- app/(tabs)/wallet.tsx
+++ app/(tabs)/wallet.tsx
@@ -1,15 +1,86 @@
 import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import { useRouter } from "expo-router";
+import * as Icons from "phosphor-react-native";
 import React from "react";
-import { StyleSheet, Text } from "react-native";
-
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 const Wallet = () => {
+  const router = useRouter();
+
+  const getTotalBalance = () => {
+    return 2200;
+  };
   return (
-    <ScreenWrapper>
-      <Text>Wallet</Text>
+    <ScreenWrapper style={{ backgroundColor: colors.black }}>
+      <View style={styles.container}>
+        {/* Balance View */}
+        <View style={styles.balanceView}>
+          <View style={{ alignItems: "center" }}>
+            <Typo size={45} fontWeight={"500"}>
+              <Typo size={16}>PKR</Typo> {getTotalBalance()?.toFixed(2)}
+            </Typo>
+            <Typo size={16} color={colors.neutral300}>
+              Total Blalance
+            </Typo>
+          </View>
+        </View>
+
+        {/* All Wallets */}
+        <View style={styles.wallets}>
+          {/* Header */}
+          <View style={styles.flexRow}>
+            <Typo size={20} fontWeight={"500"}>
+              My Wallets
+            </Typo>
+            <TouchableOpacity
+              onPress={() => router.push("/(modals)/walletModal")}
+            >
+              <Icons.PlusCircleIcon
+                weight="fill"
+                color={colors.primary}
+                size={verticalScale(33)}
+              />
+            </TouchableOpacity>
+          </View>
+
+          {/* Wallets List */}
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Wallet;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  balanceView: {
+    height: verticalScale(160),
+    backgroundColor: colors.black,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  flexRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: spacingY._10,
+  },
+  wallets: {
+    flex: 1,
+    backgroundColor: colors.neutral900,
+    borderTopRightRadius: radius._30,
+    borderTopLeftRadius: radius._30,
+    padding: spacingX._20,
+    paddingTop: spacingX._25,
+  },
+  listStyle: {
+    paddingVertical: spacingY._25,
+    paddingTop: spacingY._15,
+  },
+});
