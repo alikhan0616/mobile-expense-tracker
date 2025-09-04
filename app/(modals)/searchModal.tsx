af8d9def21f7a0f3588c0a5920c576a9049ec697
alikhan0616 <ak098boy@gmail.com>
2025-09-04 19:12:38 +0500

    feat: add SearchModal component for transaction searching and integrate with navigation
    feat: App Complete

---
+++ app/(modals)/searchModal.tsx
@@ -0,0 +1,87 @@
+import BackButton from "@/components/BackButton";
+import Header from "@/components/Header";
+import Input from "@/components/Input";
+import ModalWrapper from "@/components/ModalWrapper";
+import TransactionList from "@/components/TransactionList";
+import { colors, spacingY } from "@/constants/theme";
+import { useAuth } from "@/contexts/authContext";
+import useFetchData from "@/hooks/useFetchData";
+import { TransactionType } from "@/types";
+import { orderBy, where } from "firebase/firestore";
+import React, { useState } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+const SearchModal = () => {
+  const { user } = useAuth();
+  const [search, setSearch] = useState("");
+  const constraints = [where("uid", "==", user?.uid), orderBy("date", "desc")];
+
+  const { data: allTransactions, loading: transactionsLoading } =
+    useFetchData<TransactionType>("transactions", constraints);
+  const filteredTransactions = allTransactions.filter((item) => {
+    if (search.length > 1) {
+      if (
+        item.category?.toLowerCase().includes(search?.toLowerCase()) ||
+        item.type?.toLowerCase().includes(search?.toLowerCase()) ||
+        item.description?.toLowerCase().includes(search?.toLowerCase())
+      ) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  });
+  return (
+    <ModalWrapper style={{ backgroundColor: colors.neutral900 }}>
+      <View style={styles.container}>
+        <Header
+          title={"Search"}
+          leftIcon={<BackButton />}
+          style={{ marginBottom: spacingY._10 }}
+        />
+        {/* Form component would go here */}
+        <ScrollView contentContainerStyle={styles.form}>
+          <View style={styles.inputContainer}>
+            <Input
+              placeholder="shoes..."
+              value={search}
+              placeholderTextColor={colors.neutral400}
+              containerStyle={{ backgroundColor: colors.neutral800 }}
+              onChangeText={(text) => setSearch(text)}
+            />
+          </View>
+          <View>
+            <TransactionList
+              loading={transactionsLoading}
+              data={filteredTransactions}
+              emptyListMessage="No transactions match your search keywords."
+            />
+          </View>
+        </ScrollView>
+      </View>
+    </ModalWrapper>
+  );
+};
+
+export default SearchModal;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingHorizontal: spacingY._20,
+    // paddingVertical: spacingY. 30,
+  },
+
+  form: {
+    gap: spacingY._30,
+    marginTop: spacingY._15,
+  },
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+
+  inputContainer: {
+    gap: spacingY._10,
+  },
+});
